c182415a866ab2ff669389ec282d5bb3c14ab6cb
johan <2118300577@soy.utj.edu.mx>
2021-02-26 16:11:25 -0600

    Formulario equipo

--- src/Components/Equipos.jsx
+++ src/Components/Equipos.jsx
@@ -10,8 +10,55 @@ const Equipos = () => {
     }, [])
 
     return(
+          <form class="Formulario">
+
+          <fieldset>
+            <label for="T.Equipo"> Tipo de Equipo :</label>
+            <select class="T.Equipo" name="T.Equipo">
+
+              <option value="pc">PC</option>
+              <option value="monitor">Monitor</option>
+              <option value="laptop">Laptop</option>
+
+            </select>
+
+     <br></br>
+     <br></br>
+           <label for="responsable" > Responsable : </label>
+           <input type="text" name="responsable"  autocomplete="on"></input>
+    
+    <br></br>
+    <br></br>
+          <label for="departamento" > Departamento :</label>
+          <input type="text" name="departamento"  autocomplete="on"></input>
+
+    <br></br>
+    <br></br>
+
+    <label for="sucursal" > Sucursal :</label>
+          <input type="text" name="sucursal"  autocomplete="on"></input>
+
+    <br></br>
+    <br></br>
+
+    <label for="departamento" > Departamento :</label>
+          <input type="text" name="departamento"  autocomplete="on"></input>
+
+    <br></br>
+    <br></br>
+    <label for="T.Equipo"> Tipo de Equipo :</label>
+            <select class="T.Equipo" name="T.Equipo" heigth="200">
+
+              <option value="pc">PC</option>
+              <option value="monitor">Monitor</option>
+              <option value="laptop">Laptop</option>
+
+            </select>
+
+         </fieldset>
+    </form>
         
-        <AddEquipos/>
+       // <AddEquipos/>
     )
 
 }
