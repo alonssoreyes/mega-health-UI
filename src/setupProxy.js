d35bc9871b6165cecdcc3a33b5cfbf0c1e241371
Alonso <alonssoreyes@gmail.com>
2021-03-10 20:07:18 -0600

    hardcoded api

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const {createProxyMiddleware} = require('http-proxy-middleware');
-module.exports = function(app){
-    app.use('/api', createProxyMiddleware({
-        target: process.env.REACT_APP_API_HOST || 'http://localhost:3001/',
-        changeOrigin:true
-    }))
-}
