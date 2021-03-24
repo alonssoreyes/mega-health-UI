c994f7a32cc042d9670a163e56970600e168ae74
johan <2118300577@soy.utj.edu.mx>
2021-03-24 01:47:53 -0600

    imagen login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -37,11 +37,9 @@ const Login = ({onLogin}) => {
 
   }
 
-  return (
+  return (   
 
-       
-    <div class="box">
-    <a href="#" class="box"> </a>
+    <div class="page-wrapper full-page box ">
       <div class="page-content d-flex align-items-center justify-content-center">
         <div class="row w-100 mx-0 auth-page">
           <div class="col-md-8 col-xl-6 mx-auto">
